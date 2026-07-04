90c92087fc163a489f0cceb764926cccef4a0e28
flo <sabaniflorian@gmail.com>
2026-07-04 21:34:05 +0200

    chore: update generated multilingual metadata

--- .astro/content.d.ts
+++ .astro/content.d.ts
@@ -189,6 +189,14 @@ declare module 'astro:content' {
   rendered?: RenderedContent;
   filePath?: string;
 }>;
+"pages": Record<string, {
+  id: string;
+  body?: string;
+  collection: "pages";
+  data: InferEntrySchema<"pages">;
+  rendered?: RenderedContent;
+  filePath?: string;
+}>;
 "posts": Record<string, {
   id: string;
   body?: string;
